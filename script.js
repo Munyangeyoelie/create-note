a59b7be694b8fefd79f1a153aabadb302ebc11da
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-31 11:20:20 +0300

    styling

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
@@ -15,5 +18,6 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
   }
 });
