2929ae4262c1923193ecf3e6affc5e3aec4848f4
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-31 10:15:23 +0300

    adding functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const notesContainer = document.querySelector(".note-container");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createAttribute("img");
+  inputBox.className = "input-box";
+  inputBox.setAttributeNode("contenteditable", "true");
+  img.src = "assets/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
